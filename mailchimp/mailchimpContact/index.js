1a677fa42d3bf18733f44a8c8e23663bb5817c67
brockinit <brocklanoza@gmail.com>
2017-08-18 09:23:59 -1000

    Use env variables to load list paths

--- mailchimp/mailchimpContact/index.js
+++ mailchimp/mailchimpContact/index.js
@@ -1,30 +1,38 @@
 const Mailchimp = require('mailchimp-api-v3');
 
 module.exports.handler = (event, context, callback) => {
-  const API_KEY = process.env.API_KEY;
+  const { CONTACT_LIST_ID, API_KEY } = process.env;
   const mailchimp = new Mailchimp(API_KEY);
   mailchimp
     .request({
       method: 'post',
-      path: '/lists/96d472b10a/members',
+      path: `/lists/${CONTACT_LIST_ID}/members`,
       body: {
         email_address: event.emailAddress,
         status: 'subscribed',
         merge_fields: {
           FNAME: event.firstName,
           LNAME: event.lastName,
-          NOTE: event.contactUsNote
-        }
+          NOTE: event.contactUsNote,
+        },
       },
     })
     .then(function(results) {
       callback(null, {
         statusCode: 200,
-        body: JSON.stringify({ test: 'object' }),
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ success: true }),
       });
     })
     .catch(function(err) {
       callback(err, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+        },
         statusCode: 500,
       });
     });
--- mailchimp/mailchimpDraftManual/index.js
+++ mailchimp/mailchimpDraftManual/index.js
@@ -1,31 +1,39 @@
 const Mailchimp = require('mailchimp-api-v3');
 
 module.exports.handler = (event, context, callback) => {
-  const API_KEY = process.env.API_KEY;
+  const { DRAFT_MANUAL_LIST_ID, API_KEY } = process.env;
   const mailchimp = new Mailchimp(API_KEY);
   mailchimp
     .request({
       method: 'post',
-      path: '/lists/b24d66b224/members',
+      path: `/lists/${DRAFT_MANUAL_LIST_ID}/members`,
       body: {
         email_address: event.emailAddress,
         status: 'subscribed',
         merge_fields: {
           FNAME: event.firstName,
           LNAME: event.lastName,
-          NOTE: event.contactUsNote
-        }
-      }
+          NOTE: event.contactUsNote,
+        },
+      },
     })
     .then(function(results) {
       callback(null, {
         statusCode: 200,
-        body: JSON.stringify({ test: 'object' })
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ success: true }),
       });
     })
     .catch(function(err) {
       callback(err, {
-        statusCode: 500
+        statusCode: 500,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+        },
       });
     });
 };
